4405980194ff1b81b2010cb6bee417b7d142eea1
Rayhan Adi Wicaksono <rayhan.a.wicaksono@gmail.com>
2023-11-08 22:52:52 +0700

    fix: minor update

--- Extract_Traffic.py
+++ Extract_Traffic.py
@@ -1,16 +1,16 @@
 import requests
 import pandas as pd
-import json
 
 def extract_traffic():
-    api_url = "https://api.tomtom.com/traffic/services/4/flowSegmentData/relative/10/json?key=GyrwSRUHGsEJP9wCFTGyvWx2LSCGaOtN&point=-7.761175,110.380499"  # Replace with the API URL you want to fetch data from
+    api_url = "https://api.tomtom.com/traffic/services/4/flowSegmentData/relative/10/json?key=GyrwSRUHGsEJP9wCFTGyvWx2LSCGaOtN&point=-7.761175,110.380499" 
     response = requests.get(api_url)
 
 
     if response.status_code == 200:
-        data = response.json()  # Assuming the API returns JSON data\
+        data = response.json()
         df_traffic = pd.DataFrame(data["flowSegmentData"])
     else:
         print("Failed to fetch traffic data from the API.")
     
     return df_traffic
+
--- Extract_Weather.py
+++ Extract_Weather.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 def extract_weather():
 
-    api_url = "https://api.open-meteo.com/v1/forecast?latitude=-7.761175&longitude=110.380499&current=temperature_2m,relative_humidity_2m,precipitation,rain,showers,wind_speed_10m"  # Replace with the API URL you want to fetch data from
+    api_url = "https://api.open-meteo.com/v1/forecast?latitude=-7.761175&longitude=110.380499&current=temperature_2m,relative_humidity_2m,precipitation,rain,showers,wind_speed_10m" 
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        data = response.json()  # Assuming the API returns JSON data
+        data = response.json() 
         df_weather = pd.DataFrame(data)
     else:
         print("Failed to fetch weather data from the API.")
