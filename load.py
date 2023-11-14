f6832c725cdcffa90ec7b6be5024588dd712d750
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2023-11-14 17:39:32 +0700

    fix: fix credential into remote credential

--- load.py
+++ load.py
@@ -5,6 +5,8 @@ import logging
 def load(df_traffic, df_weather):
 
     engine = create_engine('postgresql://rekdatkelompoka:rekdata@localhost/traffic-weather')
+    #engine = create_engine('postgresql://spkfqrtx:RbQ0GfHW5OXX3KRxfNKGdA8JO0d2njzh@bubble.db.elephantsql.com/spkfqrtx')
+    
     df_traffic.to_sql('traffic', engine, if_exists='append', index=False)
     df_weather.to_sql('weather', engine, if_exists='append', index=False)
     print("ETL Success")
